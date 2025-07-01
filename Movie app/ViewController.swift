6fa10d75fcef9b1b775947c195642f77034daf81
Mohamed Rizk <m.rizk@moddakir.com>
2025-07-01 17:19:40 +0300

    initial setup

---
+++ Movie app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie app
+//
+//  Created by Mohamed Rizk on 01/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
