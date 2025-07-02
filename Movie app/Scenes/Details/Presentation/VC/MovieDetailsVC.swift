76515de8c045188eccecf04f5ebf0743663c74d1
Mohamed Rizk <m.rizk@moddakir.com>
2025-07-02 12:04:40 +0300

    Add: MovieDetailsVC Design for Movie Details

--- Movie app/Scenes/Details/Presentation/VC/MovieDetailsVC.swift
+++ Movie app/Scenes/Details/Presentation/VC/MovieDetailsVC.swift
@@ -9,9 +9,25 @@ import UIKit
 
 class MovieDetailsVC: UIViewController {
 
+    @IBOutlet weak var uiMoviePoster: UIImageView!
+    @IBOutlet weak var uiMovieTitle: UILabel!
+    @IBOutlet weak var uiMovieRate: UILabel!
+    @IBOutlet weak var uiFavoriteButton: UIButton!
+    @IBOutlet weak var uiReleaseDate: UILabel!
+    @IBOutlet weak var uiVoteAverage: UILabel!
+    @IBOutlet weak var uiOrginalLanguage: UILabel!
+    @IBOutlet weak var uiMovieOverview: UILabel!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func favoriteButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func backButtonAction(_ sender: Any) {
+    }
+    
 }
